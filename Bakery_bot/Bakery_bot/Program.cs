e853e10eb65e5ea1b624333be5ed415b25ee6cf5
Dima <shkoliarenkod@gmail.com>
2023-03-05 23:43:37 +0100

    -feat: telegram bot
    Started creating telegram bot which will provide opportunity to see bakery products and buy

---
+++ Bakery_bot/Bakery_bot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bakery_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("6013304748:AAF3OYLJkQTwmrw-XgNHIpY2ZthjbQOQhB4");
+
+            client.StartReceiving(Update,Error);
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                if (message.Text.ToLower().Contains("Hello"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Hi");
+                    return;
+                }
+
+            }
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
